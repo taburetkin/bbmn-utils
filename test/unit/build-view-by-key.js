4bac1aa8c6a7de96a6ae2d2f655488be04fb967a
dimaver <dima.taburetkin@gmail.com>
2018-09-27 18:14:06 +0300

    buildViewByKey test

--- test/unit/build-view-by-key.js
+++ test/unit/build-view-by-key.js
@@ -1,31 +1,64 @@
 import '../setup';
 import { buildViewByKey, isView } from '../../src/index.js';
+import { BackboneView } from 'bbmn-core';
 
-describe('utils • build-view-by-key',function(){
-
-	describe('when key is not a string', function(){
-		it('should return undefined', function(){
-			expect(buildViewByKey()).to.be.undefined;
-			expect(buildViewByKey(() => {})).to.be.undefined;
-			expect(buildViewByKey([1,2])).to.be.undefined;
-			expect(buildViewByKey({})).to.be.undefined;
-			expect(buildViewByKey(1)).to.be.undefined;
-		});
+describe('build-view-by-key',function(){
+	const testView = new BackboneView();
+	const context = {
+		text: 'this is a text',
+		testViewClass: BackboneView,
+		testView,				
+	}
+	it('should return undefined if key is not a string', function(){
+		expect(buildViewByKey()).to.be.undefined;
+		expect(buildViewByKey(() => {})).to.be.undefined;
+		expect(buildViewByKey([1,2])).to.be.undefined;
+		expect(buildViewByKey({})).to.be.undefined;
+		expect(buildViewByKey(1)).to.be.undefined;
 	});
-	
-	describe('when context has no property', function(){
+
+	it('should return undefined if TextView not passed and value is a string', function(){
+		expect(buildViewByKey.call(context, 'text')).to.be.undefined;
+	});
+
+	it('should return view if TextView is passed and value is a string', function(){
+		expect(buildViewByKey.call(context, 'text', { TextView: BackboneView })).to.be.instanceOf(BackboneView);
+	});	
+
+	it('should return view if TextView is passed and value is a string', function(){
+		expect(buildViewByKey.call(context, 'text', { TextView: BackboneView })).to.be.instanceOf(BackboneView);
+	});		
+
+	it('should return view if value is a view', function(){
+		expect(buildViewByKey.call(context, 'testView')).to.be.equal(testView);
+	});	
+
+	it('should return view if value is a viewClass and there is no options for view in context ', function(){
+		expect(buildViewByKey.call(context, 'testViewClass')).to.be.instanceOf(BackboneView);
+	});	
+
+	describe('when ViewClass and ViewClassOptions supplied', function(){
 		let context;
-		beforeEach(function(){
-			context = {
-				options: {},
-				buildViewByKey
-			};
+		let view;
+		const viewClass = BackboneView.extend({
+			constructor(options){
+				BackboneView.apply(this, options);
+				this.options = options;					
+			}
 		});
-		it('should return undefined', function(){
-			
+		const viewClassOptions = { foo: 'bar' };
+		context = {
+			viewClass,
+			viewClassOptions
+		}
+		view = buildViewByKey.call(context, 'viewClass');
+
+		it('should return instance of context class', function(){
+			expect(view).to.be.instanceOf(BackboneView);
+		});	
+		it('should take options from context', function(){
+			expect(view.options).to.be.eql(viewClassOptions);
 		});
 	});
-
-
 
 });
